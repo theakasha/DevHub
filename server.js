b4a9fd8b9f63375c35c9d8475eec90838962b96b
theakasha <akash23aryan@gmail.com>
2020-08-27 16:07:12 +0530

    deploy1

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ app.use('/api/posts', require('./routes/api/post'));
 
 //serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
